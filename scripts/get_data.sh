9f63abfebf82dcb3b9cc60691245561786a172fb
Gustavo <gustavo@macnord.local>
2018-04-05 11:08:35 -0700

    Added script dir and script

---
+++ scripts/get_data.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+echo -n A,
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'Element=A' > /dev/null 2> /dev/null
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'name=realtime'
+echo
+echo -n B,
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'Element=B' > /dev/null 2> /dev/null
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'name=realtime'
+echo
+echo -n C,
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'Element=C' > /dev/null 2> /dev/null
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'name=realtime'
+echo
+echo -n D,
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'Element=D' > /dev/null 2> /dev/null
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'name=realtime'
+echo
+echo -n E,
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'Element=E' > /dev/null 2> /dev/null
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'name=realtime'
+echo
+echo -n F,
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'Element=F' > /dev/null 2> /dev/null
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'name=realtime'
+echo
+echo -n G,
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'Element=G' > /dev/null 2> /dev/null
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'name=realtime'
+echo
+echo -n H,
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'Element=H' > /dev/null 2> /dev/null
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'name=realtime'
+echo
+echo -n I,
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'Element=I' > /dev/null 2> /dev/null
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'name=realtime'
+echo
+echo -n J,
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'Element=J' > /dev/null 2> /dev/null
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'name=realtime'
+echo
+echo -n K,
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'Element=K' > /dev/null 2> /dev/null
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'name=realtime'
+echo
+echo -n L,
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'Element=L' > /dev/null 2> /dev/null
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'name=realtime'
+echo
+echo -n M,
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'Element=M' > /dev/null 2> /dev/null
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'name=realtime'
+echo
+echo -n N,
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'Element=N' > /dev/null 2> /dev/null
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'name=realtime'
+echo
+echo -n O,
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'Element=O' > /dev/null 2> /dev/null
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'name=realtime'
+echo
+echo -n P,
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'Element=P' > /dev/null 2> /dev/null
+curl 'http://192.168.2.73/form_return_page' -H 'Referer: http://192.168.2.73/realtime.html' --data 'name=realtime'
+echo
