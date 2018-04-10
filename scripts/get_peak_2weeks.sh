9eed156098feae23cc1547133c68c5db881860c3
Gustavo <gustavo@macnord.i4c.lan>
2018-04-10 12:54:14 -0700

    Updated get_peak_2weeks.sh

--- scripts/get_peak_2weeks.sh
+++ scripts/get_peak_2weeks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "select d1.* from ( select timestamp, Element, VA1, VA2, VA3, TRUNCATE((VA1+VA2+VA3)/1000, 2) as kVA from data where Element='A' and timestamp >= DATE_SUB(curdate(), INTERVAL 14 DAY) order by timestamp desc ) d1 order by d1.kVA desc limit 1;" | mysql ammeter
+echo "select d1.* from ( select timestamp, Element, VA1, VA2, VA3, TRUNCATE((VA1+VA2+VA3)/1000, 2) as kVA from data where Element='A' and timestamp >= DATE_SUB(curdate(), INTERVAL 14 DAY) order by timestamp desc ) d1 order by d1.kVA desc limit 1;" | mysql --table ammeter
 
 for i in B C D E F G H I J K L M N O P; do
 
